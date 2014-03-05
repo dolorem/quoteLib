98f0573456d403b6b8b9a1c81d653c7fb82e7d73
Mateusz Śmiech <msmiech0@gmail.com>
2014-03-05 13:16:43 +0100

    Framework files - out-of-box Kohana and Bootstrap.

---
+++ application/logs/2014/03/05.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2014-03-05 06:13:31 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /var/www/system/classes/Kohana/Cookie.php:67
+2014-03-05 06:13:31 --- DEBUG: #0 /var/www/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('__atuvc', NULL)
+#1 /var/www/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('__atuvc')
+#2 /var/www/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /var/www/system/classes/Kohana/Cookie.php:67
+2014-03-05 06:13:35 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /var/www/system/classes/Kohana/Cookie.php:67
+2014-03-05 06:13:35 --- DEBUG: #0 /var/www/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('__atuvc', NULL)
+#1 /var/www/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('__atuvc')
+#2 /var/www/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /var/www/system/classes/Kohana/Cookie.php:67
+2014-03-05 06:13:36 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /var/www/system/classes/Kohana/Cookie.php:67
+2014-03-05 06:13:36 --- DEBUG: #0 /var/www/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('__atuvc', NULL)
+#1 /var/www/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('__atuvc')
+#2 /var/www/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /var/www/system/classes/Kohana/Cookie.php:67
